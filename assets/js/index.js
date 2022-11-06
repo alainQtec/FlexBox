ded5b70a14b6da9abaa2640dba7008892fe53ed8
Alain Herve <alain.1337dev@outlook.com>
2022-11-06 17:45:44 +0200

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,52 @@
 const routes = {
   404: {
     title: "404",
-    template: "views/404.html",
+    template: "/views/404.html",
     Description: "404 Page Not Found",
   },
   "/": {
     title: "Home",
-    template: "views/home.html",
+    template: "/views/home.html",
     Description: "This is the Home Page",
   },
   "/about": {
     title: "About",
-    template: "views/about.html",
+    template: "/views/about.html",
     Description: "This is the about Page",
   },
   "/contact": {
     title: "Contact",
-    template: "views/contact.html",
+    template: "/views/contact.html",
     Description: "This is the Contact Page",
   },
 };
-function handleUrlLocation() {
-  const location = window.location.pathname;
-  console.log("handleUrlLocation", location);
-}
-function urlRouter(e) {
-  const event = e || window.event;
+
+// EventListeners
+document.addEventListener("click", (event) => {
+  if (!event.target.matches("nav a")) {
+    return;
+  }
   event.preventDefault();
-  console.log("urlRouter", event.target);
+  window.route(e);
+});
+
+// Functions
+async function handleUrlLocation() {
+  let location = window.location.pathname;
+  if (location === "/index.html" || location.length == 0) {
+    location = "/";
+  }
+  console.log("handleUrlLocation", location);
+  const route = routes[location] || routes[404];
+  console.log(route.template);
+  const html = await fetch(route.template).then((res) => res.text);
+  console.log(html);
 }
 
-window.route = urlRouter;
+window.route = (e = window.event) => {
+  e.preventDefault();
+  window.history.pushState({}, "", e.target.href);
+  handleUrlLocation();
+};
 window.onpopstate = handleUrlLocation;
 handleUrlLocation();
