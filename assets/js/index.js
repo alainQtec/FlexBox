06a03fb294ff99d517092f5d45b3b1594e8746c8
Alain Herve <alain.1337dev@outlook.com>
2022-11-06 16:55:22 +0200

    Create index.js

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+const routes = {
+  404: {
+    title: "404",
+    template: "views/404.html",
+    Description: "404 Page Not Found",
+  },
+  "/": {
+    title: "Home",
+    template: "views/home.html",
+    Description: "This is the Home Page",
+  },
+  "/about": {
+    title: "About",
+    template: "views/about.html",
+    Description: "This is the about Page",
+  },
+  "/contact": {
+    title: "Contact",
+    template: "views/contact.html",
+    Description: "This is the Contact Page",
+  },
+};
+
+console.log(routes);
